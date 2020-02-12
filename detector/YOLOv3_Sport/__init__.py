10ae62801be8399f09ee7a2cc31cfec5836611d4
Stuart Morgan <stuart.morgan@ausport.gov.au>
2020-02-12 16:53:25 +1100

    Upated import paths.

---
+++ detector/YOLOv3_Sport/__init__.py
@@ -0,0 +1,9 @@
+# import sys
+# sys.path.append("detector/YOLOv3_Sport")
+#
+# from .detector import YOLOv3_Sport
+# __all__ = ['YOLOv3_Sport']
+
+from .sport_detector import YOLOv3_4_SPORT
+
+__all__ = ['YOLOv3_4_SPORT']
