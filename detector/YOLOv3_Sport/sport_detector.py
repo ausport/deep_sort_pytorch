1e853a47a12b87fffbfedbe40d94d155871adb22
Stuart Morgan <stuart.morgan@ausport.gov.au>
2020-02-13 13:36:39 +1100

    Translated Yolo detections from sport-trained model.

--- detector/YOLOv3_Sport/sport_detector.py
+++ detector/YOLOv3_Sport/sport_detector.py
@@ -10,12 +10,19 @@ import cv2
 from .models import *
 from .helpers.utils import *
 from .helpers.datasets import *
-# from .helpers.utils import
 
-# from darknet import Darknet
-# from yolo_utils import get_all_boxes, nms, post_process, xywh_to_xyxy, xyxy_to_xywh
-# from nms import boxes_nms
+def xyxy_to_xywh(boxes_xyxy):
+	if isinstance(boxes_xyxy, torch.Tensor):
+		boxes_xywh = boxes_xyxy.clone()
+	elif isinstance(boxes_xyxy, np.ndarray):
+		boxes_xywh = boxes_xyxy.copy()
 
+	boxes_xywh[:, 0] = (boxes_xyxy[:, 0] + boxes_xyxy[:, 2]) / 2.
+	boxes_xywh[:, 1] = (boxes_xyxy[:, 1] + boxes_xyxy[:, 3]) / 2.
+	boxes_xywh[:, 2] = boxes_xyxy[:, 2] - boxes_xyxy[:, 0]
+	boxes_xywh[:, 3] = boxes_xyxy[:, 3] - boxes_xyxy[:, 1]
+
+	return boxes_xywh
 
 class YOLOv3_4_SPORT(object):
 	def __init__(self, cfgfile, weightfile, namesfile, score_thresh=0.7, conf_thresh=0.01, nms_thresh=0.45,
@@ -49,8 +56,7 @@ class YOLOv3_4_SPORT(object):
 		self.conf_thresh = conf_thresh
 		self.nms_thresh = nms_thresh
 		self.use_cuda = use_cuda
-	# 	self.is_xywh = is_xywh
-	# 	self.num_classes = self.net.num_classes
+		self.num_classes = 1
 		self.class_names = self.load_class_names(namesfile)
 	#
 	def __call__(self, ori_img):
@@ -61,28 +67,22 @@ class YOLOv3_4_SPORT(object):
 		img = cv2.resize(img, self.size)
 		img = torch.from_numpy(img).float().permute(2, 0, 1).unsqueeze(0)
 
-		print(img)
-		sys.exit(1)
-		# forward
+		# Get detections
 		with torch.no_grad():
 			img = img.to(self.device)
-			out_boxes = self.net(img)
-			boxes = get_all_boxes(out_boxes, self.conf_thresh, self.num_classes,
-			                      use_cuda=self.use_cuda)  # batch size is 1
-			# boxes = nms(boxes, self.nms_thresh)
-
-			boxes = post_process(boxes, self.net.num_classes, self.conf_thresh, self.nms_thresh)[0].cpu()
+			detections = self.net(img)
+			boxes = non_max_suppression(detections, self.conf_thresh, self.nms_thresh)[0].cpu()
 			boxes = boxes[boxes[:, -2] > self.score_thresh, :]  # bbox xmin ymin xmax ymax
 
 		if len(boxes) == 0:
 			return None, None, None
 
 		height, width = ori_img.shape[:2]
-		bbox = boxes[:, :4]
-		if self.is_xywh:
-			# bbox x y w h
-			bbox = xyxy_to_xywh(bbox)
+		height /= 608
+		width /= 608
 
+		bbox = boxes[:, :4]
+		bbox = xyxy_to_xywh(bbox)
 		bbox = bbox * torch.FloatTensor([[width, height, width, height]])
 		cls_conf = boxes[:, 5]
 		cls_ids = boxes[:, 6].long()
